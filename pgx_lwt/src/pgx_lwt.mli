411c8a53d7a4bf1e4dbaa29d376570416624af6a
Brendan Long <self@brendanlong.com>
2018-03-08 14:41:08 -0500

    Give with_conn all of the constructor options
    Previously it only took ?database, but since this is our preferred
    interface, we should accept all of the options.

--- pgx_lwt/src/pgx_lwt.mli
+++ pgx_lwt/src/pgx_lwt.mli
@@ -4,5 +4,3 @@ include (Pgx.S with type 'a monad = 'a Lwt.t)
 
 (* for testing purposes *)
 module Thread : Pgx.IO with type 'a t = 'a Lwt.t
-
-val with_conn: ?database:string -> (t -> 'b Lwt.t) -> 'b Lwt.t
