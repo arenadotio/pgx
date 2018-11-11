7e8f3b6383630d90f375ccfffe3f5c55b9014d8b
Brendan Long <self@brendanlong.com>
2018-11-11 11:58:14 -0500

    Run everything through ocp-indent

--- pgx_unix/src/pgx_unix.mli
+++ pgx_unix/src/pgx_unix.mli
@@ -17,7 +17,7 @@
  * along with this library; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
- *)
+*)
 
 
 include Pgx.S with type 'a monad = 'a
