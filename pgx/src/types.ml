c69b413854fa683e7ac9b09a5933c709ad42f398
Petter A. Urkedal <paurkedal@gmail.com>
2019-11-06 21:55:15 +0100

    Fix warnings and deprecations under 4.08.
      - Fix ambiguous documentation comments.
      - Avoid deprecated Pervasives module.

--- pgx/src/types.ml
+++ pgx/src/types.ml
@@ -3,7 +3,9 @@ open Sexplib0.Sexp_conv
 type oid = int32 [@@deriving sexp]
 
 type param = Pgx_value.t [@@deriving sexp_of] (** None is NULL. *)
+
 type result = Pgx_value.t [@@deriving sexp_of] (** None is NULL. *)
+
 type row = Pgx_value.t list [@@deriving sexp_of] (** One row is a list of fields. *)
 
 type params_description = oid list [@@deriving sexp]
