0fa60f549a08fa509bdd3b9ac9c911a70354d528
Petter A. Urkedal <paurkedal@gmail.com>
2019-11-26 20:38:20 +0100

    Convert tests to alcotest and mostly limit ppx_jane to ppx_sexp_conv.

--- pgx_lwt/test/test_pgx_lwt.ml
+++ pgx_lwt/test/test_pgx_lwt.ml
@@ -1,5 +1,6 @@
-include Pgx_test.Make_tests (Pgx_lwt.Thread)
+module Alcotest_io = struct
+  let test_case name speed f = Alcotest_lwt.test_case name speed (fun _ -> f)
+end
+include Pgx_test.Make_tests (Pgx_lwt.Thread) (Alcotest_io)
 
-let () =
-  run_tests ()
-  |> Lwt_main.run
+let () = run_tests ()
