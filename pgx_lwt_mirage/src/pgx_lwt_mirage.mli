0ae443bd8431aebd6504a5e43b1da1ba1f917365
Anurag Soni <anurag@sonianurag.com>
2020-05-02 15:37:54 -0400

    add pgx_mirage

---
+++ pgx_lwt_mirage/src/pgx_lwt_mirage.mli
@@ -0,0 +1,28 @@
+(* Draft for cherry-picking into pgx.
+ *
+ * Copyright (C) 2020 Petter A. Urkedal
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version,
+ * with the OCaml static compilation exception.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *)
+
+module Make : functor
+  (RANDOM : Mirage_random.S)
+  (CLOCK : Mirage_clock.MCLOCK)
+  (STACK : Mirage_stack.V4)
+  -> sig
+  val create : STACK.t -> (module Pgx_lwt.S.Pgx_lwt)
+end
