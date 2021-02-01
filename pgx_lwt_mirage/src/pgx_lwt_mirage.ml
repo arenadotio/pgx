21699f6afacb1d75768cabed52a4e0880b85d621
Thomas Gazagnaire <thomas@gazagnaire.org>
2021-02-01 22:43:00 +0100

    Adapt to conduit 2.3

--- pgx_lwt_mirage/src/pgx_lwt_mirage.ml
+++ pgx_lwt_mirage/src/pgx_lwt_mirage.ml
@@ -18,65 +18,10 @@
  *)
 
 open Lwt.Infix
-module Channel = Mirage_channel.Make (Conduit_mirage.Flow)
 
 (* Defining this inline so we can use older lwt versions. *)
 let ( let* ) = Lwt.bind
 let ( let+ ) t f = Lwt.map f t
-
-module Thread = struct
-  type sockaddr =
-    | Unix of string
-    | Inet of string * int
-
-  type in_channel = Channel.t
-  type out_channel = Channel.t
-
-  let output_char oc c =
-    Channel.write_char oc c;
-    Lwt.return_unit
-  ;;
-
-  let output_string oc s =
-    Channel.write_string oc s 0 (String.length s);
-    Lwt.return_unit
-  ;;
-
-  let flush oc =
-    Channel.flush oc
-    >>= function
-    | Ok () -> Lwt.return_unit
-    | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_write_error err)
-  ;;
-
-  let input_char ic =
-    Channel.read_char ic
-    >>= function
-    | Ok (`Data c) -> Lwt.return c
-    | Ok `Eof -> Lwt.fail End_of_file
-    | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_error err)
-  ;;
-
-  let really_input ic buf off len =
-    Channel.read_exactly ~len ic
-    >>= function
-    | Ok (`Data bufs) ->
-      let content = Cstruct.copyv bufs in
-      Bytes.blit_string content 0 buf off len;
-      Lwt.return_unit
-    | Ok `Eof -> Lwt.fail End_of_file
-    | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_error err)
-  ;;
-
-  let close_in oc =
-    Channel.close oc
-    >>= function
-    | Ok () -> Lwt.return_unit
-    | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_write_error err)
-  ;;
-
-  let getlogin () = Lwt.fail_with "Running under MirageOS. getlogin not available."
-end
 
 module Make
     (RANDOM : Mirage_random.S)
@@ -84,15 +29,72 @@ module Make
     (MCLOCK : Mirage_clock.MCLOCK)
     (STACK : Mirage_stack.V4) =
 struct
+  module Channel = Mirage_channel.Make (STACK.TCPV4)
+
+  module Thread = struct
+    type sockaddr =
+      | Unix of string
+      | Inet of string * int
+
+    type in_channel = Channel.t
+    type out_channel = Channel.t
+
+    let output_char oc c =
+      Channel.write_char oc c;
+      Lwt.return_unit
+    ;;
+
+    let output_string oc s =
+      Channel.write_string oc s 0 (String.length s);
+      Lwt.return_unit
+    ;;
+
+    let flush oc =
+      Channel.flush oc
+      >>= function
+      | Ok () -> Lwt.return_unit
+      | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_write_error err)
+    ;;
+
+    let input_char ic =
+      Channel.read_char ic
+      >>= function
+      | Ok (`Data c) -> Lwt.return c
+      | Ok `Eof -> Lwt.fail End_of_file
+      | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_error err)
+    ;;
+
+    let really_input ic buf off len =
+      Channel.read_exactly ~len ic
+      >>= function
+      | Ok (`Data bufs) ->
+        let content = Cstruct.copyv bufs in
+        Bytes.blit_string content 0 buf off len;
+        Lwt.return_unit
+      | Ok `Eof -> Lwt.fail End_of_file
+      | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_error err)
+    ;;
+
+    let close_in oc =
+      Channel.close oc
+      >>= function
+      | Ok () -> Lwt.return_unit
+      | Error err -> Lwt.fail_with (Format.asprintf "%a" Channel.pp_write_error err)
+    ;;
+
+    let getlogin () = Lwt.fail_with "Running under MirageOS. getlogin not available."
+  end
+
   module Dns = Dns_client_mirage.Make (RANDOM) (TIME) (MCLOCK) (STACK)
 
   type sockaddr = Thread.sockaddr =
     | Unix of string
     | Inet of string * int
 
+  module TCP = Conduit_mirage.TCP (STACK)
+
   let connect_stack stack sockaddr =
     let dns = Dns.create stack in
-    let* conduit = Conduit_mirage.(with_tcp empty (stackv4 (module STACK)) stack) in
     let* client =
       match sockaddr with
       | Unix _ -> Lwt.fail_with "Running under MirageOS. Unix sockets are not available."
@@ -106,7 +108,7 @@ struct
           | Ok ipaddr -> Lwt.return (`TCP (Ipaddr.V4 ipaddr, port))
           | Error (`Msg msg) -> Lwt.fail_with msg))
     in
-    let+ flow = Conduit_mirage.connect conduit client in
+    let+ flow = TCP.connect stack client in
     let ch = Channel.create flow in
     ch, ch
   ;;
