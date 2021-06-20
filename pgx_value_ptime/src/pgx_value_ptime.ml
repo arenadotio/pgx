8539d4a4946af04ee213c0b633d96a21edb710bb
Joseph Thomas <jsthomas@protonmail.com>
2021-06-19 20:51:23 -0600

    Continue improving unit tests.

--- pgx_value_ptime/src/pgx_value_ptime.ml
+++ pgx_value_ptime/src/pgx_value_ptime.ml
@@ -1,33 +1,27 @@
 include Pgx.Value
 
 
-let of_date d =
-  let (year, month, day) = d in
+let of_date (year, month, day) =
   Printf.sprintf "%04d-%02d-%02d" year month day |> Pgx.Value.of_string
 
-
 let to_date' text =
-  match String.split_on_char '-' text with
-  | [y; m; d] -> (int_of_string y, int_of_string m, int_of_string d)
-  | _ -> convert_failure "time" text
-
+  match text ^ "T00:00:00Z" |> Ptime.of_rfc3339 with
+  | Result.Ok (t, _, _) -> Ptime.to_date t
+  | _ -> convert_failure "date" text
 
 let to_date_exn v = Pgx.Value.to_string_exn v |> to_date'
-
-
 let to_date v = Pgx.Value.to_string v |> Option.map to_date'
 
 
 let of_time ?tz_offset_s t =
-  let offset = Option.value tz_offset_s ~default:0 in
-  Ptime.to_rfc3339 ~tz_offset_s:offset ~frac_s:12 t |> Pgx.Value.of_string
+  let tz_offset_s = Option.value tz_offset_s ~default:0 in
+  Ptime.to_rfc3339 ~tz_offset_s ~frac_s:12 t |> Pgx.Value.of_string
 
 
 let time_of_string text =
   match Ptime.of_rfc3339 text with
-    | Result.Ok (t, None, _) -> (t, 0)
-    | Result.Ok (t, Some s, _) -> (t, s)
-    | _ -> convert_failure "time" text
+  | Result.Ok (t, offset, _) -> (t, Option.value ~default:0 offset)
+  | _ -> convert_failure "time" text
 
 
 let to_time' text =
@@ -49,17 +43,14 @@ let to_time' text =
   let open Re in
   let tz = seq [ alt [ char '-'; char '+' ]; digit; digit ] in
   let utctz = seq [ char 'Z'; eol ] |> compile in
-  let localtz_no_min = seq [ tz; eol ] |> compile in
   let localtz = seq [ tz; char ':'; digit; digit; eol ] |> compile in
-  Printf.printf "A: %s\n" text;
+  let localtz_no_min = seq [ tz; eol ] |> compile in
   time_of_string
     @@
     match matches utctz text, matches localtz text, matches localtz_no_min text with
-    | [], [], [] -> Printf.printf "%s" "B\n"; text ^ "Z"
-    | _, [], [] -> Printf.printf "%s" "C\n"; text
-    | [], _, [] -> Printf.printf "%s" "D\n"; text
-    | [], [], _ -> Printf.printf "%s" "E\n"; text ^ ":00"
-    (* It either finishes in one of the patterns above or it doesn't *)
+    | [], [], [] -> text ^ "Z"
+    | _, _, [] -> text
+    | [], [], _ -> text ^ ":00"
     | _ -> convert_failure "time" text
 
 
--- pgx_value_ptime/test/test_pgx_value_ptime.ml
+++ pgx_value_ptime/test/test_pgx_value_ptime.ml
@@ -1,41 +1,74 @@
 module Value = Pgx_value_ptime
 
-let time_roundtrip str = Value.of_string str |> Value.to_time_exn
-
-
-(* Show both an easy to interpret version of the date and the underlying seconds/offset pair for the input datetime.*)
-let printer (t, tz_offset_s) =
+(* Show both an human-readable version of the date and the underlying
+   seconds/offset pair for the input datetime.*)
+let print_time (t, tz_offset_s) =
   let sec = Ptime.to_float_s t
   and txt = Ptime.to_rfc3339 t ~tz_offset_s ~frac_s:6 in
   Printf.sprintf "<%s | Seconds: %f, Offset: %d>" txt sec tz_offset_s
 
-
-(* Just present the easy to interpret version *)
-let simple_printer (t, tz_offset_s) =
-  Ptime.to_rfc3339 t ~tz_offset_s ~frac_s:6
-
-
-let compare_times (t1, o1) (t2, o2) =
-  let tf1 = Ptime.to_float_s t1
-  and tf2 = Ptime.to_float_s t2 in
-  (abs_float (tf1 -. tf2) < 1.0e-6)  && (o1 = o2)
+let value_testable =
+  let print_time value = match Pgx.Value.to_string value with
+    | Some text -> text
+    | None -> "<None>" in
+  let formatter ppf value = Format.pp_print_string ppf (print_time value) in
+  Alcotest.testable formatter (=)
 
 
-let time_testable =
-  Alcotest.testable (fun ppf t -> Format.pp_print_string ppf (printer t)) compare_times
+let check_value = Alcotest.check value_testable
 
 
-let check_time = Alcotest.check time_testable
-let check_string = Alcotest.(check string)
+let test_to_date _ =
+  let check_date = Alcotest.(check (triple int int int)) in
+  let value = Pgx.Value.of_string "2021-11-14" in
+  let expected = (2021, 11, 14) in
+  check_date "check date parsing" expected (Value.to_date_exn value);
+  let value = Pgx.Value.of_string "0900-06-13" in
+  let expected = (900, 6, 13) in
+  check_date "check date with leading zeros" expected (Value.to_date_exn value)
+
+
+let test_of_date _ =
+  let date = (2021, 11, 14) in
+  let expected = Pgx.Value.of_string "2021-11-14" in
+  check_value "check date rendering" expected (Value.of_date date);
+  let date = (900, 6, 13) in
+  let expected = Pgx.Value.of_string "0900-06-13" in
+  check_value "dates with leading zeros render properly" expected (Value.of_date date)
+
+
+let date_tests =
+  [
+    Alcotest.test_case "of_date renders a Ptime date to a Pgx Value" `Quick test_of_date;
+    Alcotest.test_case "to_date parses a Pgx Value to a Ptime date" `Quick test_to_date
+  ]
+
+
+(* Show only the human-readable version of the date. *)
+let check_time =
+  let compare_times (t1, o1) (t2, o2) =
+    let tf1 = Ptime.to_float_s t1
+    and tf2 = Ptime.to_float_s t2 in
+    (abs_float (tf1 -. tf2) < 1.0e-6) && (o1 = o2) in
+  let time_testable =
+    Alcotest.testable (fun ppf t -> Format.pp_print_string ppf (print_time t)) compare_times in
+  Alcotest.check time_testable
 
 
 let test_time_of_string _ =
+  let hour = 3600 in
+  let hourf = 3600. in
+  let to_pt x = Ptime.of_float_s x |> Option.value ~default:Ptime.min in
   check_time "minimum time parses" (Ptime.min, 0) (Value.time_of_string "0000-01-01T00:00:00Z");
-  let pt = Option.value ~default:Ptime.min @@ Ptime.of_float_s @@ 12. *. 3600. in
-  check_time "time with tz offset parses" (pt, ~-4 * 3600) (Value.time_of_string "1970-01-01T08:00:00-04:00");
-  let pt = Option.value ~default:Ptime.min @@ Ptime.of_float_s @@ 12. *. 3600. +. 0.12345 in
-  check_time "a time with milliseconds parses" (pt, 0) (Value.time_of_string "1970-01-01T12:00:00.12345Z")
+  let pt = to_pt (12. *. hourf) in
+  check_time "time with tz offset parses" (pt, ~-4 * hour) (Value.time_of_string "1970-01-01T08:00:00-04:00");
+  let pt = to_pt (12. *. hourf +. 0.12345) in
+  check_time "a time with milliseconds parses" (pt, 0) (Value.time_of_string "1970-01-01T12:00:00.12345Z");
+  (* On linux run TZ='UTC' date -d @1458086118 to confirm this conversion is correct.*)
+  check_time "a recent time parses" (to_pt 1458086118., ~-4 * hour) (Value.time_of_string "2016-03-15 19:55:18-04:00")
 
+
+let time_roundtrip str = Value.of_string str |> Value.to_time_exn
 
 let test_time_tz_handling _ =
   let (utc_t, tz_offset_s) = Value.time_of_string "2016-03-15 19:55:18-04:00" in
@@ -46,8 +79,9 @@ let test_time_tz_handling _ =
 
 
 let test_time_conversion_roundtrip _ =
+  let print_time (t, tz_offset_s) = Ptime.to_rfc3339 t ~tz_offset_s ~frac_s:6 in
   let expected_str = "2016-03-15T23:55:18.123456Z" in
-  check_string "parse-print" expected_str (time_roundtrip expected_str |> simple_printer);
+  Alcotest.(check string) "parse-print" expected_str (time_roundtrip expected_str |> print_time);
   let (t, tz_offset_s) = Value.time_of_string expected_str in
   let actual = Value.of_time t ~tz_offset_s |> Value.to_time_exn in
   check_time "print-parse" (t, tz_offset_s) actual
@@ -66,45 +100,7 @@ let time_tests =
   ]
 
 
-let date_testable =
-  Alcotest.testable (fun ppf t -> Format.pp_print_string ppf (printer t)) (=)
-
-
-let check_date = Alcotest.(check (triple int int int))
-
-let value_testable =
-  let printer value = match Pgx.Value.to_string value with
-    | Some text -> text
-    | None -> "<None>" in
-  let formatter ppf value = Format.pp_print_string ppf (printer value) in
-  Alcotest.testable formatter (=)
-
-
-let check_value: string -> Value.t -> Value.t -> unit = Alcotest.check value_testable
-
-
-let test_to_date _ =
-  let value = Pgx.Value.of_string "2021-11-14" in
-  let expected = (2021, 11, 14) in
-  check_date "check date parsing" expected (Value.to_date_exn value);
-  let value = Pgx.Value.of_string "0900-06-13" in
-  let expected = (900, 6, 13) in
-  check_date "check date with leading zeros" expected (Value.to_date_exn value)
-
-
-let test_of_date _ =
-  let date = (2021, 11, 14) in
-  let expected = Pgx.Value.of_string "2021-11-14" in
-  check_value "check date rendering" expected (Value.of_date date);
-  let date = (900, 6, 13) in
-  let expected = Pgx.Value.of_string "0900-06-13" in
-  check_value "dates with leading zeros render properly" expected (Value.of_date date)
-
-let date_tests =
-  [
-    Alcotest.test_case "of_date renders a Ptime date to a Pgx Value" `Quick test_of_date;
-    Alcotest.test_case "to_date parses a Pgx Value to a Ptime date" `Quick test_to_date
-  ]
-
-
-let () = Alcotest.run "pgx_async_conversions" [ "date", date_tests; "time", time_tests]
+let () = Alcotest.run "pgx_async_conversions" [
+  "date", date_tests;
+  "time", time_tests
+]
